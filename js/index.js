f98cb52d1414a2c206a34414dd67add84239d0a7
Иванов Владислав <arshavin0309@mail.ru>
2024-05-20 16:04:27 +0500

    add

--- js/index.js
+++ js/index.js
@@ -8,3 +8,28 @@ const swiper = new Swiper('.swiper', {
         clickable: true
     },
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    let goTopBtn = document.querySelector('.upButton');
+
+    window.addEventListener('scroll', trackScroll);
+    goTopBtn.addEventListener('click', backToTop);
+
+    function trackScroll() {
+        let scrolled = window.pageYOffset;
+
+        if (scrolled > 100) {
+            goTopBtn.classList.add('show');
+        }
+        if (scrolled < 100) {
+            goTopBtn.classList.remove('show');
+        }
+    }
+
+    function backToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+})
